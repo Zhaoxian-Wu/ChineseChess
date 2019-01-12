31d29b95c6b6f1065bb593a744b1c0bef5fbe06f
MrFive5555 <936811731@qq.com>
2019-01-12 11:26:11 +0800

    update test file

--- alphabeta/test_alphabeta.py
+++ alphabeta/test_alphabeta.py
@@ -15,7 +15,7 @@ cb = [
 
 def test(cb, nextPace, title, depth=4):
   p = pace.getPace(cb, depth, '0000')
-  if p != nextPace:
+  if not p in nextPace:
     print('[{}] Failed'.format(title))
     print('[expect]', nextPace)
     print('[answer]', p)
@@ -37,7 +37,7 @@ def test(cb, nextPace, title, depth=4):
       [None,None,None,None,None,None,None,None,None],
       [None,None,None,None,None,None,None,None,None]
     ],
-    'nextPace': (5, 3, 6, 1)
+    'nextPace': [(5, 3, 6, 1)]
   },
 '''
 input = [
@@ -55,23 +55,8 @@ input = [
       [None,None,None,None,None,None,None,None,None],
       [None,None,None,None,None,None,None,None,None]
     ],
-    'nextPace': (5, 3, 6, 1)
-  },
-  {
-    'title': 'test2',
-    'cb': [
-      ['C0',None,'X0','S0','J0','S1','X1',None,None],
-      [None,None,None,None,None,None,None,None,None],
-      [None,'P0','M0',None,'P1',None,'M1',None,None],
-      ['Z0',None,'Z1',None,'Z2',None,'Z3',None,'Z4'],
-      [None,None,None,None,None,None,None,'C0',None],
-      [None,None,'z1',None,None,None,None,None,None],
-      ['z0',None,None,None,'z2',None,'z3',None,'z4'],
-      [None,'m0','p0',None,None,None,'m1','p1',None],
-      [None,None,None,None,None,'c0',None,None,None],
-      [None,None,'x0','s0','j0','s1','x1','c1',None]
-    ],
-    'nextPace': (5, 3, 6, 1)
+    'nextPace': [(5, 3, 6, 1), (5, 3, 7, 2)],
+    'depth': 3,
   },
   {
     'title': 'steb',
@@ -87,7 +72,7 @@ input = [
       [None,None,None,None,None,None,None,None,None],
       [None,None,None,None,'j0',None,None,None,None]
     ],
-    'nextPace': (4, 0, 4, 9)
+    'nextPace': [(4, 0, 4, 9)]
   },
   {
     'title': 'deading_1',
@@ -103,7 +88,7 @@ input = [
       ['P0',None,None,None,'s0',None,'c0',None,None],
       [None,None,None,'s1','j0',None,None,None,None]
     ],
-    'nextPace': (0, 8, 0, 9)
+    'nextPace': [(0, 8, 0, 9)]
   },
   {
     'title': 'eat_che',
@@ -119,7 +104,7 @@ input = [
       [None,None,None,None,None,'c0',None,'C1',None],
       [None,None,None,'s0','j0',None,None,'c1',None]
     ],
-    'nextPace': (7, 8, 7, 9)
+    'nextPace': [(7, 8, 7, 9)]
   },
   {
     'title': 'take_che',
@@ -135,12 +120,12 @@ input = [
       [None,None,None,None,None,'c0',None,'m0',None],
       [None,None,None,None,'j0',None,None,'c1',None]
     ],
-    'nextPace': (0, 0, 0, 9)
+    'nextPace': [(0, 0, 0, 9)]
   },
   {
-    'title': 'take_che',
+    'title': 'take_che2',
     'cb': [
-      [None,None,None,'c1',None,None,None,None,None],
+      [None,None,None,None,'z2',None,None,None,None],
       ['P0',None,'P1',None,None,'J0',None,None,None],
       [None,None,None,'z0','X0','z1',None,None,'X0'],
       [None,None,None,None,None,None,None,None,None],
@@ -151,10 +136,13 @@ input = [
       [None,None,None,None,None,None,None,None,None],
       [None,None,None,None,None,'j0',None,None,None]
     ],
-    'nextPace': (5, 1, 4, 1)
+    'nextPace': [(5, 1, 4, 1)]
   },
 ]
 
 if __name__ == '__main__':
   for i in input:
-    test(i['cb'], i['nextPace'], i['title'])
+    if 'depth' in i:
+      test(i['cb'], i['nextPace'], i['title'], depth=i['depth'])
+    else:
+      test(i['cb'], i['nextPace'], i['title'])
